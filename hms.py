a555ce642b5b8677ee928fc399b419d5ef33ade8
panjabi-misba <panjabimisba@gmail.com>
2026-09-11 09:29:59 +0530

    v1.0 - basic issue and return

---
+++ hms.py
@@ -0,0 +1,8 @@
+# Hostel Management System - Version 1.0
+# Feature: allot and vacate rooms
+
+def allot_room(room_id, student_id):
+    print("Room", room_id, "allotted to student", student_id)
+
+def vacate_room(room_id):
+    print("Room", room_id, "vacated")
