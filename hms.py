f52f4a8cacad5788873597d6eb1457cfe1c1779a
panjabi-misba <panjabimisba@gmail.com>
2026-09-11 09:35:21 +0530

    v2.0 - added online catalogue

--- hms.py
+++ hms.py
@@ -1,5 +1,7 @@
-# Hostel Management System - Version 1.1
-# Features: allot and vacate rooms, fine calculation
+# Hostel Management System - Version 2.0
+# Features: allot/vacate rooms, fine calculation, online room search
+
+rooms = ["Room 101", "Room 102", "Room 103"]
 
 def allot_room(room_id, student_id):
     print("Room", room_id, "allotted to student", student_id)
@@ -11,3 +13,9 @@ def calculate_fine(days_late, rate=50):
     fine = days_late * rate
     print("Fine = Rs.", fine)
     return fine
+
+def search_room(room_id):
+    if room_id in rooms:
+        print(room_id, "is available")
+    else:
+        print(room_id, "not found")
